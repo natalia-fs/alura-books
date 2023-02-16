0a3c6ec918041b817348dc0ac4e322c3b0573793
Natalia <natalia-321s@outlook.com>
2023-02-16 13:30:00 -0300

    Logo do AluraBooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-
+import logoImg from './img/logo.svg';
 function App() {
   return (
-    <div>
-      <header>
-
+    <div className='App'>
+      <header className='App-header'>
+        <div className='logo'>
+          <img src={logoImg} alt="Logo da Alura Books"/>
+          <p><strong>Alura</strong>Books</p>
+        </div>
       </header>
     </div>
   );
